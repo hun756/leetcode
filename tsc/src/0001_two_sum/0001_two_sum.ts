3a01536e3d8794ef75bc11a64bcbf289967ba231
Mehmet Şükrü Ekemen <56188619+hun756@users.noreply.github.com>
2021-07-19 16:19:41 +0000

    Fixed testing error

--- tsc/src/0001_two_sum/0001_two_sum.ts
+++ tsc/src/0001_two_sum/0001_two_sum.ts
@@ -1,4 +1,4 @@
-export class Solution {
+export default class Solution {
     twoSum(nums: number[], target: number): number[] {
         for (let i: number = 0; (i < nums.length); i++) {
             for (let j: number = (i + 1); (j < nums.length); j++) {
---
+++ tsc/tests/two_sum.test.ts
@@ -0,0 +1,33 @@
+import { expect } from 'chai'
+import Solution from '../src/0001_two_sum/0001_two_sum';
+
+describe('0001TwoSumSolutionTest', () => {
+    it('firstTest', () => {
+        let inputArr: number[] = [2, 7, 11, 15];
+        let target: number = 9;
+        let actual: number[] = [0, 1];
+
+        let solution = new Solution();
+        expect(solution.twoSum(inputArr, target)).deep.equal(actual);
+    })
+
+
+    it('SecondTest', () => {
+        let inputArr: number[] = [3, 2, 4];
+        let target: number = 6;
+        let actual: number[] = [1, 2];
+
+        let solution = new Solution();
+        expect(solution.twoSum(inputArr, target)).deep.equal(actual);
+    })
+
+
+    it('ThirdTest', () => {
+        let inputArr: number[] = [3, 3];
+        let target: number = 6;
+        let actual: number[] = [0, 1];
+
+        let solution = new Solution();
+        expect(solution.twoSum(inputArr, target)).deep.equal(actual);
+    })
+});
